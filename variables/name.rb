a3155036763b042a5600859874f7472c603dc58c
George Hockett <gwhockett@gmail.com>
2020-11-27 14:17:21 -0500

    variables exercises

---
+++ variables/name.rb
@@ -0,0 +1,11 @@
+puts "Please enter your first name:"
+name1 = gets.chomp
+
+puts "Please enter your last name:"
+name2 = gets.chomp
+
+puts "Hello " + name1 + " " + name2 + ", welcome to Ruby."
+
+10.times do
+  puts name1
+end
